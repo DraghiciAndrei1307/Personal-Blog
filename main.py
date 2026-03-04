81676ca8d4c04fb2ac8d701f3dacfbd2bbe25d58
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-04 18:14:49 +0200

    feat: update all the functions with the specific protection mechanisms

--- main.py
+++ main.py
@@ -147,10 +147,9 @@ def get_all_posts():
 
 # Allow logged-in users to comment on posts
 @app.route("/post/<int:post_id>")
-@login_required
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
-    return render_template("post.html", post=requested_post)
+    return render_template("post.html", post=requested_post, logged_in = current_user.is_authenticated)
 
 def admin_only(f):
     @wraps(f)
@@ -166,6 +165,7 @@ def admin_only(f):
 
 @app.route("/new-post", methods=["GET", "POST"])
 @admin_only
+@login_required
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -180,12 +180,13 @@ def add_new_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
+    return render_template("make-post.html", form=form, logged_in = current_user.is_authenticated)
 
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 @admin_only
+@login_required
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
@@ -203,11 +204,12 @@ def edit_post(post_id):
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
-    return render_template("make-post.html", form=edit_form, is_edit=True)
+    return render_template("make-post.html", form=edit_form, is_edit=True, logged_in = current_user.is_authenticated)
 
 
 @app.route("/delete/<int:post_id>")
 @admin_only
+@login_required
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
@@ -217,12 +219,12 @@ def delete_post(post_id):
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", logged_in = current_user.is_authenticated)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", logged_in = current_user.is_authenticated)
 
 
 if __name__ == "__main__":
