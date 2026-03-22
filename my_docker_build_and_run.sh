277e7e6e3670997c2c75374174d848033a6697cf
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-22 12:16:34 +0200

    feat(add_docker): add Docker file and bash scripts for container basic management

--- my_docker_build_and_run.sh
+++ my_docker_build_and_run.sh
@@ -2,14 +2,20 @@
 
 APP_VERSION="1.0"
 APP_NAME="personal-website-1"
+HOST_OUTPUT="/home/student/my_docker/personal-website-1/output"
+HOST_LOGS="/home/student/my_docker/personal-website-1/logs"
+CONTAINER_PORT=5000
+HOST_PORT=5000
 
 docker build -t ${APP_NAME}:${APP_VERSION} .
 
 docker run -d \
   --name ${APP_NAME}-${APP_VERSION} \
-  -v /home/student/my_docker/personal-website-1/output:/app/output \
-  -v /home/student/my_docker/personal-website-1/logs:/app/logs \
+  -p ${HOST_PORT}:${CONTAINER_PORT}
+  -v ${HOST_OUTPUT}:/app/output\
+  -v  ${HOST_LOGS}:/app/logs\
   ${APP_NAME}:${APP_VERSION}
+  venv/bin/flask run --host=0.0.0.0 --port=${CONTAINER_PORT}
 
 docker image ls ${APP_NAME}
 
